ee481b05457bbc754f2fe5847009c15dbe63977c
poojaya <83125968+poojaya@users.noreply.github.com>
2024-11-27 03:02:06 +1100

    Constructors
    Default Constructor
    Parameterized Constructor

--- src/Ride.java
+++ src/Ride.java
@@ -5,4 +5,13 @@ public class Ride {
     private String name;
     private int maxRiders;
     private Employee operator;
+    // Default Constructor
+    public Ride() {}
+
+    // Parameterized Constructor
+    public Ride(String name, int maxRiders, Employee operator) {
+        this.name = name;
+        this.maxRiders = maxRiders;
+        this.operator = operator;
+    }
 }
