dac893f05bd9e9844efb968f509fecc737439a4b
poojaya <83125968+poojaya@users.noreply.github.com>
2024-11-27 02:59:08 +1100

    Getters and Setters methods

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,5 +1,33 @@
+package src;
 // Visitor class extending Person
+
 public class Visitor extends Person {
     private String ticketId;
     private boolean isVip;
+
+        // Default Constructor
+    public Visitor() {}
+
+    // Parameterized Constructor
+    public Visitor(String name, int age, String gender, String ticketId, boolean isVip) {
+        super(name, age, gender);
+        this.ticketId = ticketId;
+        this.isVip = isVip;
+}
+    // Getters and Setters
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public boolean isVip() {
+        return isVip;
+    }
+
+    public void setVip(boolean vip) {
+        isVip = vip;
+    }
 }
