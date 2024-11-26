97733834ca02841fd6f42963bf7dd26e03bb325f
poojaya <83125968+poojaya@users.noreply.github.com>
2024-11-27 02:32:59 +1100

    constructors
    Default Constructor
    Parameterized Constructor

--- Employee.java
+++ Employee.java
@@ -1,5 +1,16 @@
 // Employee class extending Person
+import src.Person;
 public class Employee extends Person {
     private String employeeId;
     private String role;
+
+    // Default Constructor
+    public Employee() {}
+
+    // Parameterized Constructor
+    public Employee(String name, int age, String gender, String employeeId, String role) {
+        super(name, age, gender);
+        this.employeeId = employeeId;
+        this.role = role;
+}
 }
